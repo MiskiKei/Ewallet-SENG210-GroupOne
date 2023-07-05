d24813ccc09406c7d6fbe19738a5723d01603ce7
AndyLewis15 <114292647+AndyLewis15@users.noreply.github.com>
2023-07-04 20:19:00 -0400

    Update ReportExporter.java
    slight changes. Will update further when the reports have been created.

--- ReportExporter.java
+++ ReportExporter.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 public class ReportExporter extends JFrame implements ActionListener {
     private JComboBox<String> reportComboBox;
     private JButton exportButton;
+    private String monthlyIncomeReport;
+    private String monthlyExpenseReport;
 
     public ReportExporter() {
         setTitle("Report Exporter");
@@ -15,7 +17,7 @@ public class ReportExporter extends JFrame implements ActionListener {
 
         // Report Label and Combo Box
         JLabel reportLabel = new JLabel("Select Report:");
-        String[] reports = {"Report 1", "Report 2", "Report 3"}; // Replace with actual report names
+        String[] reports = {"Monthly Income Report", "Monthly Expense Report"};
         reportComboBox = new JComboBox<>(reports);
 
         // Export Button
@@ -30,7 +32,7 @@ public class ReportExporter extends JFrame implements ActionListener {
 
         pack();
         setVisible(true);
-        setSize(400,75);
+        setSize(400, 150);
         setLocationRelativeTo(null);
     }
 
@@ -38,15 +40,18 @@ public class ReportExporter extends JFrame implements ActionListener {
         if (e.getSource() == exportButton) {
             String selectedReport = (String) reportComboBox.getSelectedItem();
 
-            switch (selectedReport) { //change to actual report names
-                case "Report 1":
-                    exportReport1();
+            switch (selectedReport) {
+                case "Monthly Income Report":
+                    if (monthlyIncomeReport == null) {
+                        monthlyIncomeReport = generateMonthlyIncomeReport();
+                    }
+                    exportToFile("MonthlyIncomeReport.txt", monthlyIncomeReport);
                     break;
-                case "Report 2":
-                    exportReport2();
-                    break;
-                case "Report 3":
-                    exportReport3();
+                case "Monthly Expense Report":
+                    if (monthlyExpenseReport == null) {
+                        monthlyExpenseReport = generateMonthlyExpenseReport();
+                    }
+                    exportToFile("MonthlyExpenseReport.txt", monthlyExpenseReport);
                     break;
                 default:
                     JOptionPane.showMessageDialog(this, "Invalid report selected.");
@@ -54,19 +59,37 @@ public class ReportExporter extends JFrame implements ActionListener {
         }
     }
 
-    private void exportReport1() {
-        String reportContent = "This is Report 1 content."; // Replace with actual report content
-        exportToFile("Report1.txt", reportContent);
+    private String generateMonthlyIncomeReport() {
+        StringBuilder report = new StringBuilder();
+        // Logic to generate the monthly income report without modifying the MonthlyIncomeTracker class
+
+        report.append("Monthly Income Report:\n");
+        report.append("----------------------\n");
+        report.append("Month: January\n");
+        report.append("Salary: $3000\n");
+        report.append("Bonus: $500\n");
+        report.append("----------------------\n");
+        report.append("Overall Total Income: $3500\n");
+        report.append("----------------------\n");
+
+        return report.toString();
     }
 
-    private void exportReport2() {
-        String reportContent = "This is Report 2 content."; // Replace with actual report content
-        exportToFile("Report2.txt", reportContent);
-    }
+    private String generateMonthlyExpenseReport() {
+        StringBuilder report = new StringBuilder();
+        // Logic to generate the monthly expense report without modifying the MonthlyExpenseTracker class
 
-    private void exportReport3() {
-        String reportContent = "This is Report 3 content."; // Replace with actual report content
-        exportToFile("Report3.txt", reportContent);
+        // Example:
+        report.append("Monthly Expense Report:\n");
+        report.append("----------------------\n");
+        report.append("Month: January\n");
+        report.append("Expense 1: $100\n");
+        report.append("Expense 2: $200\n");
+        report.append("----------------------\n");
+        report.append("Total Monthly Expense: $300\n");
+        report.append("----------------------\n");
+
+        return report.toString();
     }
 
     private void exportToFile(String fileName, String content) {
@@ -74,17 +97,15 @@ public class ReportExporter extends JFrame implements ActionListener {
             FileWriter writer = new FileWriter(fileName);
             writer.write(content);
             writer.close();
-            JOptionPane.showMessageDialog(this, "Report exported successfully to " + fileName,"", JOptionPane.PLAIN_MESSAGE);
+            JOptionPane.showMessageDialog(this, "Report exported successfully to " + fileName,
+                    "", JOptionPane.PLAIN_MESSAGE);
         } catch (IOException e) {
-            JOptionPane.showMessageDialog(this, "Error exporting report: " + e.getMessage(),"", JOptionPane.PLAIN_MESSAGE);
+            JOptionPane.showMessageDialog(this, "Error exporting report: " + e.getMessage(),
+                    "", JOptionPane.PLAIN_MESSAGE);
         }
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                new ReportExporter();
-            }
-        });
+        SwingUtilities.invokeLater(ReportExporter::new);
     }
 }
